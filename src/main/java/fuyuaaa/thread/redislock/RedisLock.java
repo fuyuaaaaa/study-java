8428d7b3974d4f760f2cb8168589206690aa66ee
fuyuaaa <fuyu03@souche.com>
2019-01-28 16:05:22 +0800

    @UPDATE redis lock fix

--- src/main/java/fuyuaaa/thread/redislock/RedisLock.java
+++ src/main/java/fuyuaaa/thread/redislock/RedisLock.java
@@ -2,80 +2,101 @@ package fuyuaaa.thread.redislock;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.Collections;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
 
 /**
  * @author: fuyuaaa
- * @creat: 2019-01-24 16:29
+ * @creat: 2019-01-28 13:59
  */
-public abstract class RedisLock implements Lock {
-    Jedis jedis;
-    String lockKey;
-    String lockValue;
-    boolean isOpenFlushLeaseTime = false;
+public class RedisLock {
 
-    RedisLock(Jedis jedis, String lockKey, String lockValue) {
+    private ThreadLocal<String> tokenMap = new ThreadLocal<>();
+
+    private Jedis jedis;
+
+    private boolean isFlushLeaseTime = false;
+
+    public RedisLock(Jedis jedis) {
         this.jedis = jedis;
-        this.lockKey = lockKey;
-        this.lockValue = lockValue;
     }
 
+    public void lock(String key) {
+        lock(key,10,10);
+    }
 
-    void openFlushLeaseTime() {
+    public void lock(String key, Integer waitTime, Integer leaseTime) {
+        tryLock(key, waitTime, leaseTime);
+    }
+
+    public boolean tryLock(String key, Integer leaseTime) {
+        String result = jedis.set(key, getToken(), "NX", "EX", leaseTime);
+        System.out.println(result);
+        return "O".equals(result);
+    }
+
+    public boolean tryLock(String key, Integer waitTime, Integer leaseTime) {
+        String jedisToken = jedis.get(key);
+        if (null != jedisToken && jedisToken.equals(getToken())) {
+            System.out.println("重入锁成功 ：" + getToken());
+            return true;
+        }
+        long startTime = System.currentTimeMillis();
+        while ((System.currentTimeMillis() - startTime) / 1000 < waitTime) {
+            String result = jedis.set(key, getToken(), "NX", "EX", leaseTime);
+            if ("OK".equals(result)) {
+                System.out.println("获取锁成功" + getToken());
+                isFlushLeaseTime = true;
+                flushLeaseTime(key, getToken(), leaseTime);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void unlock(String key) {
+        String delScript = "local r = redis.call('get',KEYS[1])\n" +
+                "if r == ARGV[1]\n" +
+                "  then\n" +
+                "  redis.call('del',KEYS[1])\n" +
+                "  return true\n" +
+                "end \n" +
+                "return false";
+        Long result = (Long) jedis.eval(delScript, Collections.singletonList(key), Collections.singletonList(getToken()));
+        if (null != result && result > 0) {
+            System.out.println("remove tokenMap : " + tokenMap.get());
+            isFlushLeaseTime = false;
+            tokenMap.remove();
+        }
+    }
+
+    private void flushLeaseTime(String key, String value, Integer leaseTime) {
+        System.out.println(leaseTime.toString());
         new Thread(() -> {
-            while (isOpenFlushLeaseTime) {
-                System.out.println("执行延迟失效时间中...");
-
-                String checkAndExpireScript = "if redis.call('get', KEYS[1]) == ARGV[1] then " +
+            while (isFlushLeaseTime) {
+                try {
+                    Thread.sleep(leaseTime / 4 * 3 * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                String flushScript = "if redis.call('get', KEYS[1]) == ARGV[1] then " +
                         "return redis.call('expire',KEYS[1],ARGV[2]) " +
                         "else " +
                         "return 0 end";
-                jedis.eval(checkAndExpireScript, 1, lockKey, lockValue, "30");
-
-                sleepBySecond(10);
-
+                Long result = (Long) jedis.eval(flushScript, 1, key, value, leaseTime.toString());
+                if (1 == result) {
+                    System.out.println("刷新过期时间成功" + value);
+                }
             }
         }).start();
     }
 
-    void sleepBySecond(int second) {
-        try {
-            Thread.sleep(second * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    private String getToken() {
+        String token = tokenMap.get();
+        if (null == token || token.length() < 1) {
+            token = UUID.randomUUID().toString().replaceAll("-", "");
+            tokenMap.set(token);
         }
-    }
-
-    @Override
-    public void lock() {
-
-    }
-
-    @Override
-    public void lockInterruptibly() throws InterruptedException {
-
-    }
-
-    @Override
-    public boolean tryLock() {
-        return false;
-    }
-
-    @Override
-    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        return false;
-    }
-
-    @Override
-    public void unlock() {
-
-    }
-
-    @Override
-    public Condition newCondition() {
-        return null;
+        return token;
     }
 }
--- src/main/java/fuyuaaa/thread/redislock/RedisLockTest.java
+++ src/main/java/fuyuaaa/thread/redislock/RedisLockTest.java
@@ -3,43 +3,38 @@ package fuyuaaa.thread.redislock;
 import redis.clients.jedis.Jedis;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author: fuyuaaa
- * @creat: 2019-01-25 14:14
+ * @creat: 2019-01-28 14:06
  */
 public class RedisLockTest {
 
-    static {
-
-    }
+    private static final ExecutorService EXECUTOR_SERVICE =
+            new ThreadPoolExecutor(4, 10, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10));
 
     public static void main(String[] args) {
-        ThreadPoolExecutor poolExecutor =
-                new ThreadPoolExecutor(10, 10, 1, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10));
-
-        for (int i = 0; i < 10; i++) {
-            poolExecutor.execute(() -> {
+        for (int i = 0; i < 4; i++) {
+            EXECUTOR_SERVICE.execute(()->{
+                System.out.println("初始化。。");
                 Jedis jedis = new Jedis("106.14.169.161", 6379);
                 jedis.auth("Fuyu742423672");
                 jedis.connect();
+                RedisLock lock = new RedisLock(jedis);
+                String key = "TEST_KEY";
+                lock.lock(key,20,10);
                 try {
-                    RedisLockFullV lock = new RedisLockFullV(jedis, "test", String.valueOf(Thread.currentThread().getId()));
-                    lock.lock();
-                    lock.sleepBySecond(15);
-                    lock.unlock();
-                } catch (Exception e) {
+                    Thread.sleep(15 * 1000);
+                } catch (InterruptedException e) {
                     e.printStackTrace();
-                } finally {
-                    jedis.disconnect();
                 }
+                lock.unlock(key);
+                jedis.disconnect();
             });
         }
-        while (poolExecutor.getActiveCount() == 0) {
-            poolExecutor.shutdown();
-        }
-
+        EXECUTOR_SERVICE.shutdown();
     }
 }
