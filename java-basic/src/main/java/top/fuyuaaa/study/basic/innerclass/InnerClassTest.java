e0fe86cb27e3fc0d18936f4764f4b7e56aad6f30
fuyuaaa <fuyu03@souche.com>
2020-05-20 15:48:49 +0800

    change Inner class test

--- java-basic/src/main/java/top/fuyuaaa/study/basic/innerclass/InnerClassTest.java
+++ java-basic/src/main/java/top/fuyuaaa/study/basic/innerclass/InnerClassTest.java
@@ -1,5 +1,8 @@
 package top.fuyuaaa.study.basic.innerclass;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 /**
  * 内部类
  * Java 类中不仅可以定义变量和方法，还可以定义类，这样定义在类内部的类就被称为内部类。根
@@ -12,7 +15,8 @@ package top.fuyuaaa.study.basic.innerclass;
 public class InnerClassTest {
 
     private static String staticPrivateField = "staticPrivateField";
-    private static void staticPrivateMethod(){
+
+    private static void staticPrivateMethod() {
 
     }
 
@@ -22,9 +26,57 @@ public class InnerClassTest {
      */
     public static class StaticInnerClass {
 
-        public void testMethod(){
+        public void testMethod() {
             System.out.println(staticPrivateField);
             staticPrivateMethod();
         }
     }
+
+    /**
+     * 成员内部类
+     * 不能定义静态属性和静态方法, 但是可以定义 final static 属性
+     */
+    public class MemberInnerClass {
+
+        //如果是编译期初始化的final属性就可以存在于成员内部类
+        //不可以这样写
+        //private static String staticField = "";
+        private final static String finalStaticField = "";
+//        private final static String finalStaticField2 = "" + Math.random();
+
+        public void testMethod() {
+            System.out.println(staticPrivateField);
+            staticPrivateMethod();
+        }
+    }
+
+    /**
+     * 局部内部类
+     * 定义在方法里的内部类
+     */
+    public void testMethodInnerClass() {
+        class methodInnerClass {
+
+        }
+    }
+
+    /**
+     * 匿名内部类
+     */
+    @SuppressWarnings("unchecked")
+    public void testUnnamedInnerClass() {
+        ArrayList<Integer> arrayList = new ArrayList<>(10);
+        arrayList.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 0;
+            }
+        });
+
+
+        Thread thread = new Thread(){
+
+        };
+    }
+
 }
