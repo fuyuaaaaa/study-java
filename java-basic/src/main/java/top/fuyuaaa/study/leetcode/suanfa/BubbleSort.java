08bfdb80a685d8c41c713c4b5591d397b07511cb
fuyuaaa <fuyu03@souche.com>
2021-03-05 14:51:20 +0800

    forget

--- java-basic/src/main/java/top/fuyuaaa/study/leetcode/suanfa/BubbleSort.java
+++ java-basic/src/main/java/top/fuyuaaa/study/leetcode/suanfa/BubbleSort.java
@@ -8,20 +8,22 @@ package top.fuyuaaa.study.leetcode.suanfa;
  **/
 public class BubbleSort {
     public static void main(String[] args) {
-        int a[] = {1, 5, 2, 8, 1, 6, 8, 7, 2, 6};
-        bubbleSort(a);
-        for (int i : a) {
-            System.out.println(i);
+        String s = "codeleet";
+        int[] indices = new int[]{4, 5, 6, 7, 0, 2, 1, 3};
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < indices.length; i++) {
+            sb.append(s.charAt(indices[i]));
         }
+        System.out.println(sb.toString());
     }
 
-    public static void bubbleSort(int []a){
+    public static void bubbleSort(int[] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = 1; j < a.length; j++) {
-                if (a[j] > a[j-1]) {
+                if (a[j] > a[j - 1]) {
                     int temp = a[j];
-                    a[j] = a[j-1];
-                    a[j-1] = temp;
+                    a[j] = a[j - 1];
+                    a[j - 1] = temp;
                 }
             }
         }
